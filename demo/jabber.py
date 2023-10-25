e1b4c9d739afcb355944d2addf685f10cc2d030a
Mohamad El Moheb <krb3ym@virginia.edu>
2023-10-24 20:20:58 -0400

    package

---
+++ demo/jabber.py
@@ -0,0 +1,3 @@
+def hello():
+    '''Says Hello World'''
+    print('Hello my friend')
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+	name = 'Demo',
+	version='0.1',
+	description = 'A simple package',
+	url='https://github.com/melmoheb/my_package',
+	author='Mohamad El Moheb',
+	author_email='krb3ym@virginia.edu',
+	license='GNU',
+	packages=['demo'],
+)
